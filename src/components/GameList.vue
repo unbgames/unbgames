04c14a87525f1d426707afc5a70b24251310ed08
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-21 17:19:52 -0300

    Create home and show game views

---
+++ src/components/GameList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="game in games" :key="game.id + game.name">
+        <router-link :to="{name: 'game', params: {gameId: game.id}}">{{game.name}} - {{game.id}}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "GameList",
+  data: function() {
+    return {
+      games: []
+    };
+  },
+  mounted() {
+    var url = "http://localhost:3000/games";
+    axios
+      .get(url)
+      .then(response => {
+        this.games = response.data;
+        console.log(this.games)
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
---
+++ src/views/Game.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="game">
+    Este é o game
+    <ul>
+      <li v-for="(value, key, index) in game" :key="value+index">
+        {{key}}: {{value}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: "game",
+  components: {},
+  data: function() {
+    return {
+      game: {}
+    };
+  },
+  props: {
+    gameId: Number,
+  },
+  mounted() {
+    var url = "http://localhost:3000/games/" + this.gameId;
+    axios
+      .get(url)
+      .then(response => {
+        this.game = response.data;
+        console.log(this.game);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+};
+</script>
