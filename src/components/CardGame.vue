a39f3c67969bf0b4257ea4cdd0ad1555d97f5505
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-25 14:07:20 -0300

    Add CardGame component

---
+++ src/components/CardGame.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card>
+    <v-card-media
+        :src="cover_image"
+        height="200px"
+    ></v-card-media>
+    <v-card-title primary-title>
+        <div>
+            <h3 class="headline mb-0">{{name}}</h3>
+        </div>
+    </v-card-title>
+    <!-- <v-card-actions>
+        <v-btn flat color="orange">Share</v-btn>
+    </v-card-actions> -->
+    </v-card>  
+</template>
+
+<script>
+export default {
+    name: "CardGame",
+    props: {
+        cover_image: String,
+        name: String
+    }
+}
+</script>        
+          
