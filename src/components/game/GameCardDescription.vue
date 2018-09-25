77670a8a3dc9084f05a35b0bdbebfbfb90e03b96
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-09-25 10:38:47 -0300

    Add mock file

--- src/components/game/GameCardDescription.vue
+++ src/components/game/GameCardDescription.vue
@@ -7,14 +7,12 @@
                 <strong>Repositório Oficial:</strong><br>
                 <a class="link-app" :href="game.repository">{{game.name}}</a>
             </p>
+            <p><strong> icone - Multiplayer</strong></p>
             <p><strong>Gêneros:</strong> 
             <span v-for="(genre,i) in game.genres" :key="genre+i">
-                <v-btn color="primary" dark>
-                    {{genre}}
-                </v-btn>
+                <v-btn round small dark>{{genre}}</v-btn>
             </span>
             </p>
-            <p><strong> icone - Multiplayer</strong></p>
             <span v-for="(award,i) in game.awards" :key="award.type+i">
                 <div class="data-content" v-if="award.type=='game_year'">
                     <v-icon class="icon-content">mdi-xbox-controller</v-icon>
---
+++ utils/json-server/unbgames.js
@@ -0,0 +1,163 @@
+var faker = require("faker");
+
+function generateImages(){
+    var images = [];
+    for (var i = 0, size = 10; i < size; i++) {
+        images[i] = {
+          src: "http://lorempixel.com/640/480",
+          name: faker.name.title(),
+          is_gallery: false
+        };
+    }
+    return images;
+
+}
+
+function generateGenres(){
+    var genres = []
+    for(var i = 0; i < 5; i++){
+        genres[i] = faker.name.firstName()
+    }
+    return genres;
+}
+
+function generateAwards(){
+    return [
+        {
+          type_text: "Jogo do ano",
+          type: "game_year",
+          position: "Primeiro lugar",
+          revelation: true
+        },
+        {
+          type_text: "Melhor arte",
+          type: "art",
+          position: "Primeiro lugar",
+          revelation: true
+        },
+        {
+          type_text: "Melhor implementação",
+          type: "devel",
+          position: "Primeiro lugar",
+          revelation: true
+        },
+        {
+          type_text: "Melhor trilha sonora",
+          type: "music",
+          position: "Primeiro lugar",
+          revelation: true
+        }
+    ]
+}
+
+function generatePacks(){
+    return [
+        {
+          platform: "debian",
+          link: "http://adah.com",
+          architecture: "i386",
+          extension: "deb"
+        },
+        {
+          platform: "fedora",
+          link: "http://adah.com",
+          architecture: "x86_64",
+          extension: "rpm"
+        },
+        {
+          platform: "windows",
+          link: "http://adah.com",
+          architecture: "x86_64",
+          extension: "exe"
+        },
+        {
+          platform: "apple",
+          link: "http://adah.com",
+          architecture: "x86_64",
+          extension: "app"
+        }
+    ]
+}
+
+
+function generateCredits(){
+    var credits = []
+    var roles = ["Desenvolvedores(as)", "Artistas", "Músicos(as)"]
+    for(var i = 0; i < 3; i++){
+        credits[i] = {
+          role: roles[i],
+          authors: [
+            {
+              name: faker.name.firstName(),
+              portifolio: "http://ila.name"
+            },
+            {
+              name: faker.name.firstName(),
+              portifolio: "http://ila.name"
+            },
+            {
+              name: faker.name.firstName(),
+              portifolio: "http://ila.name"
+            }
+          ]
+        }
+    }
+
+    return credits
+}
+
+/////////////// Utils functions ///////////////////////////
+
+function generateGame(game_id) {
+    const game =
+        {
+            id: game_id,
+            name: faker.name.title(),
+            year: Math.floor(Math.random() * (2020 - 2000 +1)) + 2000,
+            cover_image: "https://raw.githubusercontent.com/unbgames/Wenova/master/res/readme/logo.png",
+            description: faker.lorem.paragraph(),
+            repository: faker.internet.url(),
+            version: 1,
+            awards: generateAwards(),
+            genres: generateGenres(),
+            packs: generatePacks(),
+            images: generateImages(),
+            //sounds: generateSounds(),
+            credits: generateCredits(),
+        };
+    return game;
+}
+
+function generateGamesYear(){
+    var games = []
+
+    for(var i = 0; i < 10; i++){
+        games[i] = generateGame(i)
+    }
+    var games_year = []
+    for(var j = 0; j < 21; j++){
+        games_year[j] = {
+            year: j+2000,
+            games: games
+        }
+    }
+    return games_year
+}
+
+function generateGames(){
+    var games = []
+
+    for(var i = 0; i < 10; i++){
+        games[i] = generateGame(i)
+    }
+    return games
+}
+
+function generateData() {
+    return {
+        games_year: generateGamesYear(),
+        games: generateGames()
+    };
+}
+
+module.exports = generateData;
