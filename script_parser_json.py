6f3a914c45440f3d1730e435e5513b380614c625
EduardoJunio <eduardojvr10@gmail.com>
2019-04-12 12:25:03 -0300

    Atualizando script de parser

--- script_parser_json.py
+++ script_parser_json.py
@@ -1,31 +1,38 @@
 import os
+import glob
 
-arquivo = open("README.md", "r")
-n_linhas = sum(1 for linha in arquivo)
-arquivo.close()
+def lista_arquivo():
+    lista = []
+    for x in glob.glob("readmes/*.md"):
+       lista.append(x)
+    return lista
 
-arquivo = open("README.md", "r")
+def salva_json(nomeArquivo):
+    os.popen('m2j '+nomeArquivo+' >> '+ nomeArquivo.replace('_tmp.md','.json'))
 
-readme_json = open('README_aux.md', 'a')
+
+lista_md = lista_arquivo()
 
 dados = []
 aux = ''
-count = 0
-readme_json.write('---\n')
-for linha in arquivo:
-    aux = linha.replace('\n', '')
-    aux = aux.replace('#', '')
-    if count == 0:
-        readme_json.write(aux+':\n')
-    else:
-        readme_json.write(aux+'\n')
-    count+=1
 
-readme_json.write('---')
+for i in lista_md:
+    count = 0
+    arquivo = open(i, "r")
+    arquivoTmp = i.replace('.md','_tmp.md')
+    readme_json = open(arquivoTmp , 'w')
+    readme_json.write('---\n')
+    for linha in arquivo:
+        aux = linha.replace('\n', '')
+        aux = aux.replace('#', '')
+        if count == 0:
+            readme_json.write(aux+':\n')
+        else:
+            readme_json.write(aux+'\n')
+        count+=1
+    readme_json.write('---')
 
-arquivo.close()
-readme_json.close()
+    arquivo.close()
+    readme_json.close()
 
-os.popen('m2j README_aux.md >> README.json')
-
-
+    salva_json(arquivoTmp)
