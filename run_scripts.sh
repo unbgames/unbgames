eb6ba32aaf507f6947a1de36b9f9dcea16077893
EduardoJunio <eduardojvr10@gmail.com>
2019-04-14 12:05:34 -0300

    Atualizando script de execução

--- run_scripts.sh
+++ run_scripts.sh
@@ -3,15 +3,18 @@ echo "Iniciando execução do script!"
 echo "Step 1"
 cd readmes/
 rm *.md
-cd json/
-rm *.json
+rm -rf json
+mkdir json 
 
 echo "Step 2"
 npm i markdown-to-json
 
 echo "Step 3"
-cd ../../
+cd ../
 python readmes/script_get_readme.py
 python script_parser_json.py
+
+echo "Step 4"
+mv readmes/*.json readmes/json
 
 echo "Fim da execução!"
