de58b47c475f59a6540cfdbe2fdfb504fa1bf828
lugope <lucasgpereira.10@gmail.com>
2019-04-24 15:01:13 -0300

    Writing dict of games info into a json.

--- concatenate_json.py
+++ concatenate_json.py
@@ -16,20 +16,7 @@ for json_file in lista_jsons:
 		game_obj = json.load(data)
 		games_list.append(game_obj)
 
-print("\n")
-for di in games_list:
-	print(di)
-
-
-# with open('readmes/json/games_db/db.json', 'w') as db:
-
-# def concatena_json():
-#     lista_jsons = lista_json()
-
-#     with open('readmes/games_db.json', 'w') as jsonDB:
-#         for json in lista_jsons:
-#             with open(json) as game_json:
-#                 data = game_json.read()
-#                 print(data)
-
-#             # game_data = json.load(data)
+# Salvando lista em um arquivo json
+with open('readmes/json/games_db/db.json', 'w') as db:
+	json.dump(games_list, db)
+	
