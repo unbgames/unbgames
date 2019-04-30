5e1e4f4439d46887f0caa1b01c9c1de8ed4ba85a
lugope <lucasgpereira.10@gmail.com>
2019-04-30 17:50:38 -0300

    Formating game database to fit the desired JSON pattern.

--- concatenate_json.py
+++ concatenate_json.py
@@ -3,14 +3,14 @@ import json
 import os
 
 # Lista JSONs dos jogos
-lista_jsons = []
-for arquivo_json in glob.glob("readmes/*.json"):
-	lista_jsons.append(arquivo_json)
+jsons_list = []
+for arquivo_json in glob.glob("readmes/json/*.json"):
+	jsons_list.append(arquivo_json)
 
 # Abre os aquivos json e coloca seus conteudos em uma lista de objetos
 games_list = []
 
-for json_file in lista_jsons:
+for json_file in jsons_list:
 	with open(json_file, "r") as data:
 		# Verifica se o arquivo já está salvo
 		while True:
@@ -29,6 +29,8 @@ for json_file in lista_jsons:
 		games_list.append(game_obj)
 
 # Salvando lista em um arquivo json que será o banco de dados
+games_db = {"jogos" : games_list}
+
 with open('utils/db.json', 'w') as db:
-	json.dump(games_list, db, indent=4)
+	json.dump(games_db, db, indent=4)
 db.close()
