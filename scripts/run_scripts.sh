cc7e6cb351083df47334b901cc893896c69dad56
Mateus Roriz <mateusvroriz6b@gmail.com>
2019-05-02 16:33:27 -0300

    Alteração nos scripts para nova estrutura de pastas e execução via ci
    Signed-off-by: Mateus Roriz <mateusvroriz6b@gmail.com>
    Co-authored-by: Caio Nunes <caiofelipe147@gmail.com>

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -8,16 +8,16 @@ rm -rf json
 mkdir json
 
 echo "Step 2"
-npm i -g markdown-to-json
-
-echo "Step 3"
 cd ../
-python readmes/script_get_readme.py
-python script_parser_json.py
+python scripts/script_get_readme.py
 sleep 3
-python concatenate_json.py
+python scripts/script_parser_json.py
+sleep 3
+python scripts/concatenate_json.py
 
 echo "Step 4"
-mv readmes/*.json readmes/json
+cd readmes/
+mv *.json json/
+ls json/
 
 echo "Fim da execução!"
--- scripts/script_get_readme.py
+++ scripts/script_get_readme.py
@@ -1,18 +1,19 @@
 from github import Github
 import urllib
+import os
 
-token = ""
+token = os.environ['AUTH_TOKEN']
 
 g = Github(token)
 org = g.get_organization('unbgames')
 
 for repo in org.get_repos():
     try:
-        file_content = repo.get_file_contents('README.md')
-        
+        file_content = repo.get_file_contents('unbgames.md')
+
         if(repo.name != 'unbgames'):
             urllib.request.urlretrieve(file_content.download_url, ('readmes/'+repo.name + '.md'))
-        
-    except:
-        print("Repository: " + repo.name + " don't have readme.md file.")
 
+    except Exception as e :
+        print("Repository: " + repo.name + " don't have readme.md file.")
+        print(str(e))
