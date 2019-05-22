6ecd3169d43990b9b323263bd8dea6ac9ff1a34d
Eduardojvr <eduardojvr10@gmail.com>
2019-05-22 15:28:18 -0300

    Adicionando modificações no script para incluir links de downloads dos pacotes de jogos

--- scripts/script_parser_json.py
+++ scripts/script_parser_json.py
@@ -41,13 +41,13 @@ for i in lista_md:
         else:
             url_rep = re.findall(r'https://', aux)
             if not url_rep:
-                if (aux.replace('#','').strip() == "Carousel Gallery:") or (aux.replace('#','').strip() == "Cover Image:") or (aux.replace('#','').strip() == "Image Gallery:"):
+                if (aux.replace('#','').strip() == "Carousel Gallery:") or (aux.replace('#','').strip() == "Cover Image:") or (aux.replace('#','').strip() == "Image Gallery:") or (aux.replace('#','').strip() == "Debian:") or (aux.replace('#','').strip() == "RedHat:") or (aux.replace('#','').strip() == "Windows:") :
                     aux = aux.replace(' ','')
                     aux = '  '+aux
 
                 if (aux.replace('#','').strip() != "Description:" and aux.replace('#','').strip() !="Version:" and aux.replace('#','').strip() !="Year:" and 
                     aux.replace('#','').strip() !="Repository:" and aux.replace('#','').strip() !="Awards:" and aux.replace('#','').strip() !="Gallery:" and
-                    aux.replace('#','').strip() !="Genre:" and aux.replace('#','').strip() !="Development:" and aux.replace('#','').strip() !="Art:" and 
+                    aux.replace('#','').strip() !="Genre:" and aux.replace('#','').strip() !="Downloads:" and aux.replace('#','').strip() !="Development:" and aux.replace('#','').strip() !="Art:" and 
                     aux.replace('#','').strip() !="Music:" and aux.replace('#','').strip() !="CoverImage:") and is_url == 0:
                     aux = aux.replace(':',' -')
 
