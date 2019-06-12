c6ef42fc349420262a51821b3df575fc73a75100
Mariana Picolo <marianapicolo@hotmail.com>
2019-06-12 02:32:55 -0300

    Update home page design

--- src/components/home/HeroComponent.vue
+++ src/components/home/HeroComponent.vue
@@ -15,12 +15,15 @@ export default {
 
 <style lang="scss" scoped>
 .header-image {
-  height: 70vh;
+  height: 75vh;
   background-image: linear-gradient(
-    to right bottom,
-    rgba(#4caf50, 1),
-    rgba(#1f4037, 1)
-  );
+      to right,
+    rgba(#449D48, 0.95),
+    rgba(#18332C, 0.95)
+    ),
+    url('https://user-images.githubusercontent.com/26073710/59325516-c3016e00-8cb9-11e9-8c65-b7e3d39dc296.png');
+  background-size: cover;
+  background-position: center;
   display: flex;
   justify-content: center;
   align-items: center;
